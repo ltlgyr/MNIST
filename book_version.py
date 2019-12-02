484b82247d3bef588cea2174fc9519863ce9d15a
tlliu <ltlgyr@163.com>
2019-12-02 18:19:17 +0800

    init

---
+++ book_version.py
@@ -0,0 +1,44 @@
+#j -*- coding: utf-8 -*-
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('./train', one_hot=True)
+
+
+'''
+    #构建运算图
+'''
+# X Y 都是占位符 占位而已 不表示具体的数据 
+x = tf.placeholder("float",[None,784]) # 图像的大小为784;None表示第一个维度可以是任意长度
+
+# 一个Variable代表一个可修改的张量,它们可以用于计算输入值，也可以在计算中被修改
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+# 计算交叉熵
+y_ = tf.placeholder("float", [None,10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y)))
+
+# 梯度下降算法（gradient descent algorithm）
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+# 在运行计算之前，我们需要添加一个操作来初始化我们创建的变量：
+init = tf.global_variables_initializer()
+
+# 在一个Session里面启动我们的模型，并且初始化变量：
+sess = tf.Session()
+sess.run(init)
+
+# 训练模型1000次
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))  
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+#print('-**-',accuracy,type(accuracy))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
---
+++ save.py
@@ -0,0 +1,20 @@
+#j -*- coding: utf-8 -*-
+from tensorflow.examples.tutorials.mnist import input_data
+import scipy.misc
+import os 
+from PIL import Image
+import numpy as np
+
+mnist = input_data.read_data_sets('./train', one_hot=True)
+save_dir= 'MNIST_DATA/raw/'
+if os.path.exists(save_dir) is False:
+	os.makedirs(save_dir)
+
+for i in range(20):
+	image_array = mnist.train.images[i,:]
+	image_array = image_array.reshape(28,28)
+	image_array = image_array * 255
+	filename = save_dir + 'mnist_train_%d.jpg' %i
+	Image.fromarray(np.uint8(image_array)).convert('RGB').save(filename)
+
+
